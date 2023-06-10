6f02b3bafe576a18ade5e9ac4a91cd545e83c9bc
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-06-10 16:21:41 +0530

    Added CS Styling and created initial JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const methods = {
+        "add": (a,b)=>a+b,
+        "subtract": (a,b)=> a-b,
+        "multiply": (a,b)=>a*b,
+        "divide": (a,b)=>a/b,
+        "remainder": (a,b)=>a%b
+    }; //An object that stores arrow functions for all mathematical operations, we will call this object to run operations
+
