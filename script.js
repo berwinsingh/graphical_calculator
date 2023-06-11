716eb607a6fa3e838bfc19b2db27e9820c53edb7
Berwin Singh <63018717+berwinsingh@users.noreply.github.com>
2023-06-12 00:38:46 +0530

    Modified HTML, CSS, and updated JS functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const numbers = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operation");
-const h2 = document.querySelector("h2");
+const h6 = document.querySelector("h6");
 const screen = document.getElementById("math-operation");
 const clear = document.getElementById("reset");
 const equal = document.getElementById("equal");
@@ -11,6 +11,11 @@ let num2 = [];
 let sign = "";
 let displaySign = "";
 
+let cal1 = "";
+let cal2 ="";
+let sol = "";
+let h6Val = "";
+
 //An object that stores arrow functions for all mathematical operations, we will call this object to run operations
 const methods = {
   add: (a, b) => a + b,
@@ -20,7 +25,7 @@ const methods = {
   remainder: (a, b) => a % b,
 };
 
-//Extracting the number 1 value from the element and displaying it inside the input
+//Extracting the number 1 & 2 array value from the element and displaying it inside the input
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", () => {
       if (sign === "") {
@@ -30,7 +35,6 @@ for (let i = 0; i < numbers.length; i++) {
       else {
         num2.push(numbers[i].textContent);
       }
-
       const joinedValue = num1.join("");
       const joinedValue2 = num2.join("");
       completeVal = joinedValue + " " + displaySign + " " + joinedValue2;
@@ -41,18 +45,36 @@ for (let i = 0; i < numbers.length; i++) {
 //Figuring out the operator that the user wants to perform and storing inside of a variable
 for (let j = 0; j < operator.length; j++) { 
   operator[j].addEventListener("click", () => {
-    if(sign===""){
-        const op = operator[j].getAttribute("id");
-        sign = op;
+    if(sign==="" && num1 != null){
+        sign = operator[j].getAttribute("id");
         displaySign = operator[j].textContent;
+        completeVal += displaySign;
+        screen.value = completeVal;
     }
-    screen.value = completeVal;
   });
 }
 
 //Adding Equal sign button functionality
 equal.addEventListener("click",()=>{
+  const callVal = completeVal.split(" ");
+  cal1 = +callVal[0];
+  cal2 = +callVal[2];
 
+  if(methods.hasOwnProperty(sign)){
+    sol = methods[sign](cal1,cal2);
+    screen.value = sol;
+    h6Val = completeVal;
+    h6.textContent = h6Val;
+    //Resetting all the other values to ensure proper multiple calculations
+    sign ="";
+    num1 = [];
+    num2 = [];
+    completeVal = "";
+    displaySign = "";
+  }
+  else{
+    console.log("invalid");
+  }
 });
 
 //Resetting Values back to 0 with AC button
@@ -63,6 +85,7 @@ clear.addEventListener("click", () => {
   completeVal = "";
   sign="";
   displaySign ="";
+  h6.textContent = "";
 });
 
 //Adding Backspace functionality
